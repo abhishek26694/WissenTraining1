5b372bba1cf2b44e47c2577c4eb249249c584f63
Abhishek Upadhyay <abhishek.upadhyay@wissen.com>
2022-12-24 22:17:22 +0530

    Updating changes

--- src/test/java/com/insurance/login/LoginApplicationTests.java
+++ src/test/java/com/insurance/login/LoginApplicationTests.java
@@ -4,20 +4,16 @@ import com.insurance.login.Service.UserService;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoginApplicationTests {
 
 	@Test
-	void validateLoginSuccessTest() {
-		/*UserService service = new UserService();
-		JSONObject userCreds = new JSONObject();
-		userCreds.put("username","Abhishek");
-		userCreds.put("password","Abhishek123");
-		JSONObject response = service.validate(userCreds);
-		assertEquals(response.get("Msg").toString(),"Success")*/;
-
+	void validateLoginSuccessTest() {;
+		assertTrue(true);
 	}
 
 }
